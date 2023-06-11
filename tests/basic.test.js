f494e2ec691cb2db80623a0b2adc222964718082
Ivan Gongora M <ivan.gongoram@gmail.com>
2023-06-10 23:44:02 -0500

    [TEST]: Test 3; function(...arg)

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -2,10 +2,15 @@ const Scheuler = require("../Scheuler");
 
 Scheuler.job(test1).everyMinute();
 Scheuler.job(test2).wait(5000).everyMinute();
+Scheuler.job(test3, 'Hello', 'world!').wait(10000).everyMinute();
 
 function test1() {
     console.log('Test 1');
 }
 function test2() {
     console.log('Test 2');
+}
+
+function test3(msg1, msg2) {
+    console.log(msg1, msg2);
 }
