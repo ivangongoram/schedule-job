a2f525150547ff05f74fc4710cbf202e50443093
Ivan Gongora M <ivan.gongoram@gmail.com>
2023-06-10 20:57:45 -0500

    [UPDATE]: Method; job - Scheuler

--- Scheuler.js
+++ Scheuler.js
@@ -2,6 +2,12 @@ class Scheuler
 {
     static callback;
     static #delay;
+
+    static job(callback){
+        this.callback = callback;
+        this.#delay = 0;
+        return this;
+    }
 }
 
 module.exports = Scheuler;
