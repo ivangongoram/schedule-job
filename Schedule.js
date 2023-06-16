54ce879a1e6fac7cc87ac37236fd07f73e13a94b
Ivan Gongora M <ivan.gongoram@gmail.com>
2023-06-15 21:29:33 -0500

    [UPDATE]: Add more frequency Options

--- Schedule.js
+++ Schedule.js
@@ -33,14 +33,44 @@ class Schedule
     static everyMinute(){
         return setTimeout(this.#mainCallback, this.delay, this.callback, 60000, this.delay, ...this.arg);
     }
+    static everyTwoMinutes(){
+        return setTimeout(this.#mainCallback, this.delay, this.callback, 120000, this.delay, ...this.arg);
+    }
+    static everyThreeMinutes(){
+        return setTimeout(this.#mainCallback, this.delay, this.callback, 180000, this.delay, ...this.arg);
+    }
+    static everyFourMinutes(){
+        return setTimeout(this.#mainCallback, this.delay, this.callback, 240000, this.delay, ...this.arg);
+    }
     static everyFiveMinutes(){
         return setTimeout(this.#mainCallback, this.delay, this.callback, 300000, this.delay, ...this.arg);
     }
     static everyTenMinutes(){
         return setTimeout(this.#mainCallback, this.delay, this.callback, 600000, this.delay, ...this.arg);
     }
+    static everyFifteenMinutes(){
+        return setTimeout(this.#mainCallback, this.delay, this.callback, 15*60*1000, this.delay, ...this.arg);
+    }
+    static everyThirtyMinutes(){
+        return setTimeout(this.#mainCallback, this.delay, this.callback, 30*60*1000, this.delay, ...this.arg);
+    }
     static everyHour(){
         return setTimeout(this.#mainCallback, this.delay, this.callback, 3600000, this.delay, ...this.arg);
+    }
+    static everyTwoHours(){
+        return setTimeout(this.#mainCallback, this.delay, this.callback, 2*60*60*1000, this.delay, ...this.arg);
+    }
+    static everyThreeHours(){
+        return setTimeout(this.#mainCallback, this.delay, this.callback, 3*60*60*1000, this.delay, ...this.arg);
+    }
+    static everyFourHours(){
+        return setTimeout(this.#mainCallback, this.delay, this.callback, 4*60*60*1000, this.delay, ...this.arg);
+    }
+    static everySixHours(){
+        return setTimeout(this.#mainCallback, this.delay, this.callback, 6*60*60*1000, this.delay, ...this.arg);
+    }
+    static daily(){
+        return setTimeout(this.#mainCallback, this.delay, this.callback, 24*60*60*1000, this.delay, ...this.arg);
     }
 }
 
