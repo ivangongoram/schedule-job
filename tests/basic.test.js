f9a976753499e5755bb0384deac3916c7c8cbd04
Ivan Gongora M <ivan.gongoram@gmail.com>
2023-07-13 19:29:37 -0500

    [TEST]: Method: everyTime

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -4,6 +4,7 @@ Schedule.job(message, 'Test', '1').everyMinute();
 Schedule.job(message, 'Test', '2').wait(5000).everyMinute();
 Schedule.job(message, 'Test', '3').wait(10000).everyFifteenMinutes();
 Schedule.job(message, 'Test', '4').wait(2000).once();
+Schedule.job(message, 'Test', '5').wait(4000).everyTime(3000);
 
 function message(msg1, msg2) {
     console.log(msg1, msg2);
